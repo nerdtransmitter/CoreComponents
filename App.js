50e49d2427758dcd558264589f77bf4d8630090f
Alessia Moison <adm2217@columbia.edu>
2024-10-07 17:55:08 +0200

    Modal

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  View,
+  Text,
+  Image,
+  ImageBackground,
+  ScrollView,
+  Button,
+  Pressable,
+  Modal
+} from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
+        <Button
+          title="Press"
+          onPress={() => console.log("Button pressed")}
+          color="midnightblue"
+        />
+      <Modal>
+        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
+          <Text> Modal content</Text>
+          <Button title="Close" color="midnightblue"/>
+        </View>
+      </Modal>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
