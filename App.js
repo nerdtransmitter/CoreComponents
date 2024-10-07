0c8cb987f2a90bfb4bb65fce324f53e7d798ec93
Alessia Moison <adm2217@columbia.edu>
2024-10-07 18:11:36 +0200

    completed modal

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   View,
   Text,
@@ -6,22 +7,31 @@ import {
   ScrollView,
   Button,
   Pressable,
-  Modal
+  Modal,
 } from "react-native";
-const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
+  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-        <Button
-          title="Press"
-          onPress={() => console.log("Button pressed")}
-          color="midnightblue"
-        />
-      <Modal>
+      <Button
+        title="Press"
+        onPress={() => setIsModalVisible(true)}
+        color="midnightblue"
+      />
+      <Modal
+        visible={isModalVisible}
+        onRequestClose={() => setIsModalVisible(false)}
+        animationType="slide"
+        presentationStyle="pageSheet"
+      >
         <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
           <Text> Modal content</Text>
-          <Button title="Close" color="midnightblue"/>
+          <Button
+            title="Close"
+            color="midnightblue"
+            onPress={() => setIsModalVisible(false)}
+          />
         </View>
       </Modal>
     </View>
