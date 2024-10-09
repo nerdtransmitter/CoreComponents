c8ddea42fbe79be4aaeafc1acc40a57e2af785c2
Alessia Moison <adm2217@columbia.edu>
2024-10-09 16:24:23 +0200

    First custom component Greet

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Greet from './components/Greet';
+
 import {
   View,
   Text,
@@ -17,51 +19,8 @@ export default function App() {
   const [isModalVisible, setIsModalVisible] = useState(false);
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <StatusBar
-        backgroundColor="lightgreen"
-        barStyle="light-content"
-        hidden={isModalVisible}
-      />
-      <Button title="Alert" onPress={() => Alert.alert("Invalid data!")} />
-      <Button
-        title="Alert 2"
-        onPress={() => Alert.alert("Invalid data!", "DOB incorrect")}
-      />
-      <Button
-        title="Alert 3"
-        onPress={() =>
-          Alert.alert("Invalid data!", "DOB incorrect", [
-            {
-              text: "Cancel",
-              onPress: () => console.log("Cancel pressed"),
-            },
-            {
-              text: "OK",
-              onPress: () => console.log("OK pressed")
-            }
-          ])
-        }
-      />
-      <Button
-        title="Press"
-        onPress={() => setIsModalVisible(true)}
-        color="midnightblue"
-      />
-      <Modal
-        visible={isModalVisible}
-        onRequestClose={() => setIsModalVisible(false)}
-        animationType="slide"
-        presentationStyle="pageSheet"
-      >
-        <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-          <Text> Modal content</Text>
-          <Button
-            title="Close"
-            color="midnightblue"
-            onPress={() => setIsModalVisible(false)}
-          />
-        </View>
-      </Modal>
+      <Greet name='Bruce Wayne' />
+      <Greet name='Clark Kent' />
     </View>
   );
 }
