d8947473f97d65c1e902bafb0f9cd8011f6948c9
Alessia Moison <adm2217@columbia.edu>
2024-10-10 14:44:02 +0200

    Starting styling using Stylesheet API

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import { useState } from "react";
-import Greet from './components/Greet';
-
-import {
-  View,
-  Text,
-  Image,
-  ImageBackground,
-  ScrollView,
-  Button,
-  Pressable,
-  Modal,
-  StatusBar,
-  ActivityIndicator,
-  Alert,
-} from "react-native";
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Greet name='Bruce Wayne' />
-      <Greet name='Clark Kent' />
+    <View style={styles.container}>
+      <Text>StyleSheet API</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create ({
+  container: { flex: 1, backgroundColor: "plum", padding: 60 }
+});
