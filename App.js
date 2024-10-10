dd86b841629f4f9c5925d779b2b615bf2be10681
Alessia Moison <adm2217@columbia.edu>
2024-10-10 15:11:55 +0200

    example of merging two styles using array syntax

--- App.js
+++ App.js
@@ -3,11 +3,27 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>StyleSheet API</Text>
+      <View style={[styles.box, styles.lightblueBg]}>
+        <Text>Lightblue box</Text>
+      </View>
+      <View style={[styles.box, styles.lightgreenBg]}>
+        <Text>Lightgreen box</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create ({
-  container: { flex: 1, backgroundColor: "plum", padding: 60 }
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  box: {
+    width: 100,
+    height: 100,
+    padding: 10,
+  },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
 });
